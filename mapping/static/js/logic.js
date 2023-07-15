b7f471fa029f0d229fc55a2a9a584da9048d182c
Craig-UO <122829965+Craig-UO@users.noreply.github.com>
2023-07-15 15:47:44 -0700

    Added map layers for in_person, hybrid, remote

--- mapping/static/js/logic.js
+++ mapping/static/js/logic.js
@@ -11,6 +11,9 @@ let baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
 
 // Create overlay layers
 let jobData = L.layerGroup().addTo(myMap);
+let inPerson = L.layerGroup().addTo(myMap);
+let hybrid = L.layerGroup().addTo(myMap);
+let remote = L.layerGroup().addTo(myMap);
 
 let markers = L.markerClusterGroup({
   showCoverageOnHover: false
@@ -36,6 +39,7 @@ d3.csv(geoData).then(function (data) {
     };
     let listing = data[i];
 
+    // Add marks for cluster map
     let clusterMarker = (L.marker([listing.lat, listing.lon]))
       .bindPopup("<h2><a href=" + listing.url + " target = '_blank'>" + listing.title + "</a></h2>" + 
       "<h3>"+listing.company+"</h3>" +
@@ -45,7 +49,7 @@ d3.csv(geoData).then(function (data) {
 
     clusterMarker.addTo(markers);
        
-
+    // Add marks for all listings
     let listingMarker = L.marker([listing.lat, listing.lon])
       .bindPopup("<h2><a href=" + listing.url + " target = '_blank'>" + listing.title + "</a></h2>" + 
         "<h3>"+listing.company+"</h3>" +
@@ -54,13 +58,53 @@ d3.csv(geoData).then(function (data) {
         "<h3>"+cleanOffice(listing.office)+"</h3>")
 
     listingMarker.addTo(jobData);
+
+    // Add marks only for in_person listings
+    if (listing.office == 'in_person') {
+      let inPersonMarker = L.marker([listing.lat, listing.lon])
+        .bindPopup("<h2><a href=" + listing.url + " target = '_blank'>" + listing.title + "</a></h2>" + 
+          "<h3>"+listing.company+"</h3>" +
+          "<h3>"+listing.location+"</h3>" +
+          "<h3>"+listing.salary+"</h3>" +
+          "<h3>"+cleanOffice(listing.office)+"</h3>")
+
+      inPersonMarker.addTo(inPerson);
+    };
+
+    // Add marks only for hybrid listings
+    if (listing.office == 'hybrid') {
+      let hybridMarker = L.marker([listing.lat, listing.lon])
+        .bindPopup("<h2><a href=" + listing.url + " target = '_blank'>" + listing.title + "</a></h2>" + 
+          "<h3>"+listing.company+"</h3>" +
+          "<h3>"+listing.location+"</h3>" +
+          "<h3>"+listing.salary+"</h3>" +
+          "<h3>"+cleanOffice(listing.office)+"</h3>")
+
+      hybridMarker.addTo(hybrid);
+    };
+
+    // Add marks only for remote listings
+    if (listing.office == 'remote') {
+      let remoteMarker = L.marker([listing.lat, listing.lon])
+        .bindPopup("<h2><a href=" + listing.url + " target = '_blank'>" + listing.title + "</a></h2>" + 
+          "<h3>"+listing.company+"</h3>" +
+          "<h3>"+listing.location+"</h3>" +
+          "<h3>"+listing.salary+"</h3>" +
+          "<h3>"+cleanOffice(listing.office)+"</h3>")
+
+      remoteMarker.addTo(remote);
+    };
+
   };  
 });
 
 // Overlay choice between cluster markers and all individual job listings
 let overlayMaps = {
   "All Listings": jobData,
-  "Cluster Map": markers
+  "In Person": inPerson,
+  "Hybrid": hybrid,
+  "Remote": remote,
+  "Cluster Map": markers,
 };
 
 // Pass our map layers into our layer control.
